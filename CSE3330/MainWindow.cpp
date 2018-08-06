b25f2a87f6c8331c0ec19a22716aeec8e4bb8ef6
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-08-06 17:33:15 -0500

    Added text to test buttons

--- CSE3330/MainWindow.cpp
+++ CSE3330/MainWindow.cpp
@@ -16,15 +16,32 @@ namespace cse3330 {
     // https://github.com/cnjinhao/nana/wiki/Using-STL-Sequence-Container
     void MainWindow::show_window() {
 
-        nana::form window;
-        nana::textbox output_textbox;
+        nana::form window{};
+        nana::textbox output_textbox{ window };
+        nana::place window_format{ window };
 
         // Text output config
         output_textbox.multi_lines(true).editable(false).line_wrapped(false);
 
         // Query buttons
+        // TODO: Get requirements from Elmasri on what queries are needed
+        nana::button
+            test_button_1{ window, "Test Button 1" },
+            test_button_2{ window, "Test Button 2" },
+            test_button_3{ window, "Test Button 3" };
 
-        // Window config
+        // Window format
+        window_format.div("<vert weight=100 <vert buttons > <> ><output>");
+
+        window_format["buttons"] 
+            << test_button_1
+            << test_button_2
+            << test_button_3;
+
+        window_format["output"] << output_textbox;
+
+        window_format.collocate();
+
         window.caption("CSE3330 Project 2");
 
         // Show the window
